e108518a98caa2ee4508a3f59402691f8fd011c8
Tarun Brari <tarun.b@outlook.com>
2019-04-24 16:02:41 +0530

    new swift codefile

---
+++ treasure.swift
@@ -0,0 +1,12 @@
+func raju() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+}
+
+#this is apple code
